ffa6ed797f77f889edbb2a5a371d2eba4830d848
Shreya Patel <shreya23kp@gmail.com>
2023-04-01 15:52:58 +0530

    New Projects

--- HibernateMapping/src/hibernate/model/Student.java
+++ HibernateMapping/src/hibernate/model/Student.java
@@ -1,14 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hibernate.model;
-
-/**
- *
- * @author SHREYA
- */
 public class Student {
     private int id;
     private String firstName;
